26cd595c889913ebe6b5ed2ef58a8d4d153457b7
florian-rosenthal <florian.rosenthal@kit.edu>
2016-02-10 16:11:53 +0000

    commit edit and editor plugin code
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/MonitorRepository/branches/extendedModel@30889 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.monitorrepository.edit/src/org/palladiosimulator/monitorrepository/provider/WindowCharacterizationItemProvider.java
@@ -0,0 +1,156 @@
+/**
+ */
+package org.palladiosimulator.monitorrepository.provider;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import org.palladiosimulator.monitorrepository.MonitorRepositoryPackage;
+import org.palladiosimulator.monitorrepository.WindowCharacterization;
+
+/**
+ * This is the item provider adapter for a
+ * {@link org.palladiosimulator.monitorrepository.WindowCharacterization} object. <!--
+ * begin-user-doc --> <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class WindowCharacterizationItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider,
+        IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+    /**
+     * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
+     * @generated
+     */
+    public WindowCharacterizationItemProvider(final AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
+
+    /**
+     * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    public List<IItemPropertyDescriptor> getPropertyDescriptors(final Object object) {
+        if (this.itemPropertyDescriptors == null) {
+            super.getPropertyDescriptors(object);
+
+            this.addWindowLengthPropertyDescriptor(object);
+            this.addWindowIncrementPropertyDescriptor(object);
+        }
+        return this.itemPropertyDescriptors;
+    }
+
+    /**
+     * This adds a property descriptor for the Window Length feature. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
+     * @generated
+     */
+    protected void addWindowLengthPropertyDescriptor(final Object object) {
+        this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(
+                ((ComposeableAdapterFactory) this.adapterFactory).getRootAdapterFactory(), this.getResourceLocator(),
+                this.getString("_UI_WindowCharacterization_windowLength_feature"),
+                this.getString("_UI_PropertyDescriptor_description", "_UI_WindowCharacterization_windowLength_feature",
+                        "_UI_WindowCharacterization_type"),
+                MonitorRepositoryPackage.Literals.WINDOW_CHARACTERIZATION__WINDOW_LENGTH, true, false, false,
+                ItemPropertyDescriptor.REAL_VALUE_IMAGE, null, null));
+    }
+
+    /**
+     * This adds a property descriptor for the Window Increment feature. <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    protected void addWindowIncrementPropertyDescriptor(final Object object) {
+        this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(
+                ((ComposeableAdapterFactory) this.adapterFactory).getRootAdapterFactory(), this.getResourceLocator(),
+                this.getString("_UI_WindowCharacterization_windowIncrement_feature"),
+                this.getString("_UI_PropertyDescriptor_description",
+                        "_UI_WindowCharacterization_windowIncrement_feature", "_UI_WindowCharacterization_type"),
+                MonitorRepositoryPackage.Literals.WINDOW_CHARACTERIZATION__WINDOW_INCREMENT, true, false, false,
+                ItemPropertyDescriptor.REAL_VALUE_IMAGE, null, null));
+    }
+
+    /**
+     * This returns WindowCharacterization.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    public Object getImage(final Object object) {
+        return this.overlayImage(object, this.getResourceLocator().getImage("full/obj16/WindowCharacterization"));
+    }
+
+    /**
+     * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
+     * -->
+     *
+     * @generated
+     */
+    @Override
+    public String getText(final Object object) {
+        final WindowCharacterization windowCharacterization = (WindowCharacterization) object;
+        return this.getString("_UI_WindowCharacterization_type") + " " + windowCharacterization.getWindowLength();
+    }
+
+    /**
+     * This handles model notifications by calling {@link #updateChildren} to update any cached
+     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}
+     * . <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    public void notifyChanged(final Notification notification) {
+        this.updateChildren(notification);
+
+        switch (notification.getFeatureID(WindowCharacterization.class)) {
+        case MonitorRepositoryPackage.WINDOW_CHARACTERIZATION__WINDOW_LENGTH:
+        case MonitorRepositoryPackage.WINDOW_CHARACTERIZATION__WINDOW_INCREMENT:
+            this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+            return;
+        }
+        super.notifyChanged(notification);
+    }
+
+    /**
+     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that
+     * can be created under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    protected void collectNewChildDescriptors(final Collection<Object> newChildDescriptors, final Object object) {
+        super.collectNewChildDescriptors(newChildDescriptors, object);
+    }
+
+    /**
+     * Return the resource locator for this item provider's resources. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    public ResourceLocator getResourceLocator() {
+        return MonitorrepositoryEditPlugin.INSTANCE;
+    }
+
+}
